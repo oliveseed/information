97d70e1a2013fc00c44090f7077abd415cd7034a
Oliver <74593676+oupak@users.noreply.github.com>
2023-12-23 19:54:11 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Oliver L`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Oliver Lin`,
+      summary: `who lives and works in Vancouver building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
